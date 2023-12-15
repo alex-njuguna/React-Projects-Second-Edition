e8d1bea8b0bee1a959c877a9973f7ccf52ab6060
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-15 17:24:11 +0300

    drag and drop functionality

---
+++ Chapter03-initial/src/pages/Backlog/Backlog.js
@@ -0,0 +1,27 @@
+import useDataFetching from '../../hooks/useDataFetching'
+import './Backlog.css'
+import Task from '../../components/Task/Task'
+
+export default function Backlog() {
+    const [loading, error, tasks] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`)
+
+    return (
+        <div className='Backlog-Wrapper'>
+            <h2>Backlog</h2>
+            <div className='Tasks-Wrapper'>
+                {loading || error ? (
+                    <span>{error || 'Loading...'}</span>
+                ): (
+                    tasks.map((task) => (
+                        <Task 
+                        key={task.id}
+                        title={task.title}
+                        body={task.body}
+                        />
+                    ))
+
+                )}
+            </div>
+        </div>
+    )
+}
--- Chapter03-initial/src/pages/Board/Board.js
+++ Chapter03-initial/src/pages/Board/Board.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
+import useDataFetching from '../../hooks/useDataFetching';
 
 const lanes = [
   { id: 1, title: 'To Do' },
@@ -9,45 +10,56 @@ const lanes = [
   { id: 4, title: 'Done' },
 ];
 
+function onDragStart(e, id) {
+  e.dataTransfer.setData('id', id)
+}
+
+function onDragOver(e) {
+  e.preventDefault()
+}
+
 function Board() {
-  const [loading, setLoading] = useState(false)
+  
+  const [loading, error, data] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`)
+
   const [tasks, setTasks] = useState([])
-  const [error, setError] = useState('')
 
-  // fetch data in useEffect with a try and catch statement
-  useEffect(() => {
-    async function fetchData(){
-      try {
-        const tasks = await fetch(
-          `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`,
-        )
-        const result = await tasks.json()
-        if (result) {
-          setTasks(result)
-          setLoading(false)
-        }
-      } catch (e) {
-        setLoading(false)
-        setError(e.message)
+  useEffect(() =>{
+    setTasks(data)
+  }, [data])
+
+  function onDrop(e, laneId){
+    const id = e.dataTransfer.getData('id')
+    const updatedTasks = tasks.filter((task) => {
+      if (task.id.toString() === id){
+        task.lane = laneId
       }
-    }
 
-    fetchData()
-  }, [])
+      return task
+    })
+    setTasks(updatedTasks)
+  }
 
   return (
     <div className='Board-wrapper'>
       {lanes.map((lane) => (
         <Lane 
         key={lane.id} 
+        laneId={lane.id}
         title={lane.title} 
         loading={loading}
         error={error}
         tasks={tasks.filter((task) => task.lane === lane.id)}
+
+        onDragStart={onDragStart}
+        onDragOver={onDragOver}
+        onDrop={onDrop}
         />
       ))}
     </div>
   );
 }
+
+
 
 export default Board;
