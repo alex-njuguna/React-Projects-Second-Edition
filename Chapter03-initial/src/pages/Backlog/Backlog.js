0d1ef6304edcc61e9bad66924f310f355e8c016a
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-15 17:37:28 +0300

    drag and drop functionality on backlog

--- Chapter03-initial/src/pages/Backlog/Backlog.js
+++ Chapter03-initial/src/pages/Backlog/Backlog.js
@@ -1,27 +1,58 @@
-import useDataFetching from '../../hooks/useDataFetching'
-import './Backlog.css'
-import Task from '../../components/Task/Task'
+import React, { useState, useEffect } from 'react';
+import useDataFetching from '../../hooks/useDataFetching';
+import './Backlog.css';
+import Task from '../../components/Task/Task';
 
-export default function Backlog() {
-    const [loading, error, tasks] = useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`)
+export default function Backlog({ onDropToBoard }) {
+  const [loading, error, tasks] = useDataFetching(
+    `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+  );
+  const [backlogTasks, setBacklogTasks] = useState([]);
 
-    return (
-        <div className='Backlog-Wrapper'>
-            <h2>Backlog</h2>
-            <div className='Tasks-Wrapper'>
-                {loading || error ? (
-                    <span>{error || 'Loading...'}</span>
-                ): (
-                    tasks.map((task) => (
-                        <Task 
-                        key={task.id}
-                        title={task.title}
-                        body={task.body}
-                        />
-                    ))
+  useEffect(() => {
+    setBacklogTasks(tasks);
+  }, [tasks]);
 
-                )}
-            </div>
-        </div>
-    )
+  function onDragStart(e, id) {
+    e.dataTransfer.setData('id', id);
+  }
+
+  function onDragOver(e) {
+    e.preventDefault();
+  }
+
+  function onDrop(e) {
+    const id = e.dataTransfer.getData('id');
+    const draggedTask = backlogTasks.find((task) => task.id.toString() === id);
+
+    // Remove the dragged task from the Backlog tasks
+    const updatedTasks = backlogTasks.filter((task) => task.id.toString() !== id);
+
+    // Notify the Board component about the drop
+    onDropToBoard(draggedTask);
+
+    // Update the Backlog tasks without the dragged task
+    setBacklogTasks(updatedTasks);
+  }
+
+  return (
+    <div className='Backlog-Wrapper' onDragOver={onDragOver} onDrop={onDrop}>
+      <h2>Backlog</h2>
+      <div className='Tasks-Wrapper'>
+        {loading || error ? (
+          <span>{error || 'Loading...'}</span>
+        ) : (
+          backlogTasks.map((task) => (
+            <Task
+              key={task.id}
+              title={task.title}
+              body={task.body}
+              draggable
+              onDragStart={(e) => onDragStart(e, task.id)}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
